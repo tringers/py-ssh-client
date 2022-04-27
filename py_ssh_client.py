e094776bfb973d655b47542c29288cfd8978ce5e
Timo Riedinger <mail@timoriedinger.de>
2022-04-27 21:30:07 +0200

    Done

--- py_ssh_client.py
+++ py_ssh_client.py
@@ -1,9 +1,8 @@
 import socket
 import sys
-import pyDH
 import rsa
-
-diffieHellman = pyDH.DiffieHellman()
+import hashlib
+from Crypto.Cipher import AES
 
 class Py_ssh_client:
 
@@ -11,64 +10,106 @@ class Py_ssh_client:
         self.generateKeys()
         self.HOST = host
         self.PORT = port
-        (self.publicKey, self.privateKey) = self.loadKeys()
-        self.sharedKey = None
+        (self.publicKey, self.privateKey, self.sharedKey) = self.loadKeys()
+        self.recievedPublicKey = None
+        self.authenticated = False
 
     def run(self):
 
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
-            s.connect((self.HOST, self.PORT))
+            sock.connect((self.HOST, self.PORT))
+
+            self.publicKeyTransaction(sock)
+            self.recieveSharedKey(sock)
 
             while True:
 
-                recivevedpub = self.recievePublicKey(s)
-                recivevedpub = str(recivevedpub).split("(")[1].split(",")
-                recivevedpub[1] = recivevedpub[1].split(")")[0].strip()
+                data = self.recieve(sock)
 
-                keyN = int(recivevedpub[0])
-                keyE = int(recivevedpub[1])
-                pub = rsa.PublicKey(keyN, keyE)
-
-                self.sendPublicKey(s, self.publicKey)
-
-                self.sharedKey = diffieHellman.gen_shared_key(
-                    pub
-                )
-
-                user_input = input()
-                s.sendall(bytes(user_input, 'utf-8'))
-                if user_input.lower() == "exit":
-                    s.close()
-                    break
-
-                data = s.recv(1024)
-
-                if data.decode("utf-8") == "auth error":
-                    s.close()
+                if data == "auth error":
+                    sock.close()
                     sys.exit("Authentifizierung fehlgeschlagen.")
+
+                print("{}".format(data))
+
+                if not self.authenticated:
+                    self.authentifizierung(sock, data)
+                else:
+                    self.commands(sock)
 
                 if not data:
                     sys.exit()
 
-                print("{}".format(data))
+    def authentifizierung(self, sock, data):
 
-    def sendPublicKey(self, sock, message):
-        sock.sendall(bytes("{}".format(message), "utf-8"))
+        if data == "User:":
+            self.userInput(sock)
+        elif data == "Password:":
+            self.userPassword(sock)
 
-    def send(self, sock, message):
-        message = rsa.encrypt(message.encode('ascii'), self.publicKey)
-        self.sendPublicKey(sock, message)
+    def publicKeyTransaction(self, sock):
+
+        recivedPublicKey = self.recievePublicKey(sock)
+        recivedPublicKey = str(recivedPublicKey).split("(")[1].split(",")
+        recivedPublicKey[1] = recivedPublicKey[1].split(")")[0].strip()
+
+        keyN = int(recivedPublicKey[0])
+        keyE = int(recivedPublicKey[1])
+
+        self.recievedPublicKey = rsa.PublicKey(keyN, keyE)
+
+        self.sendPublicKey(sock)
+
+    def userInput(self, sock):
+
+        user_input = input()
+        self.send(sock, user_input)
+
+        if user_input.lower() == "exit":
+            sock.close()
+            sys.exit("User beendet die Verbindung.")
+
+    def commands(self, sock):
+        self.userInput(sock)
+        data = self.recieve(sock)
+        print("{}".format(data.replace("\n", "")))
+
+    def userPassword(self, sock):
+
+        user_input = input()
+        password_hash = hashlib.sha512(user_input.encode("utf-8")).hexdigest()
+        self.authenticated = True
+        self.send(sock, password_hash)
+
+    def sendPublicKey(self, sock):
+        sock.sendall(bytes("{}".format(self.publicKey), "utf-8"))
 
     def recievePublicKey(self, sock):
         data = sock.recv(1024)
         return data
 
+
+    def recieveSharedKey(self, sock):
+
+        data = sock.recv(1024).decode("utf-8")
+        #data = rsa.decrypt(data, self.privateKey)
+        self.sharedKey = data
+
+
+    def send(self, sock, message):
+
+        sender = AES.new("self.sharedKey12", AES.MODE_ECB)
+        #message = sender.encrypt(message)
+        sock.sendall(bytes("{}".format(message), "utf-8"))
+
     def recieve(self, sock):
-        data = sock.recv(1024)
-        data = rsa.decrypt(data, self.sharedKey)
-        output = str(data.decode("utf-8"))
-        output = output.replace("\n", "")
+
+        data = sock.recv(1024).decode("utf-8")
+        reciever = AES.new("self.sharedKey12", AES.MODE_ECB)
+        #data = reciever.decrypt(data)
+        output = str(data).lstrip("b").strip('\'')
+        output = output.strip('\n')
         return output
 
     def generateKeys(self):
@@ -83,4 +124,6 @@ class Py_ssh_client:
             publicKey = rsa.PublicKey.load_pkcs1(p.read())
         with open('keys/privateKey.pem', 'rb') as p:
             privateKey = rsa.PrivateKey.load_pkcs1(p.read())
-        return publicKey, privateKey
+        with open('keys/sharedKey', 'rb') as p:
+            sharedKey = p.read() * 16
+        return publicKey, privateKey, sharedKey
